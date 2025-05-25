1f7be260856c9fb828e6637018b0883553f44052
pinionrack <sy2goog@gmail.com>
2025-05-25 21:47:30 +0900

    add hand landmarks collection

--- holistic.py
+++ holistic.py
@@ -1,8 +1,17 @@
 import cv2
 import mediapipe as mp
+import socket
+import json
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
+
+# UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# set server Ip address and port number
+# Ip = "127.0.0.1", Prot = 5051
+serverIpPort = ("127.0.0.1", 5051)
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
@@ -48,6 +57,39 @@ with mp_holistic.Holistic(
         mp_holistic.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles
         .get_default_pose_landmarks_style())
+
+
+    # collect hand_landmarks
+    leftHandLandmarks = []
+    if results.left_hand_landmarks:
+        for idx, lm in enumerate(results.left_hand_landmarks.landmark):
+            leftHandLandmarks.append({
+                'index': idx,
+                'x': lm.x,
+                'y': lm.y,
+                'z': lm.z
+            })
+
+    rightHandLandmarks = []
+    if results.right_hand_landmarks:
+        for idx, lm in enumerate(results.right_hand_landmarks.landmark):
+            rightHandLandmarks.append({
+                'index': idx,
+                'x': lm.x,
+                'y': lm.y,
+                'z': lm.z
+            })
+
+    # json file
+    data = json.dumps({
+        "leftHandLandmarks": leftHandLandmarks,
+        "rightHandLandmarks": rightHandLandmarks
+    })
+
+    # send data
+    sock.sendto(data.encode('utf-8'), serverIpPort)
+
+
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('Mediapipe Holistic', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
